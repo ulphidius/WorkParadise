33cb66a46bfc77679c3a188f8634a5a6e1ad5ec4
louis <louis_laurent@aol.fr>
2018-02-28 19:56:42 +0100

    modification du lien vers bootstrap et modification de la position du formulaire creation d'un fichier ajaw non fonctionnel

---
+++ php/inscription.php
@@ -0,0 +1,10 @@
+<?php
+	print("son of bitch");
+	if(isset($_POST['lastname'])
+		&& isset($_POST['firstname'])
+		&& isset($_POST['pwd'])
+		&& isset($_POST['pwd2'])
+		&& isset($_POST['email'])){
+		echo "coucou";
+	}
+?>
