ca11bba91964af0e32d9e4590b5906a1031a74b4
Dreamsplutox <arnaudsimon091@gmail.com>
2018-03-26 17:24:07 +0100

    nath back 1

---
+++ index.php
@@ -0,0 +1,125 @@
+<!doctype php>
+
+<?php 
+  session_start();  
+
+  include('utilities_functions.php');
+  $db = connectDb();
+
+  $query = $db->prepare("SELECT * FROM subscription");
+  $query->execute();
+
+  $listOfAbonnements = $query->fetchAll(PDO::FETCH_ASSOC);
+
+  ($listOfAbonnements);
+
+?>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Starter Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#"><img src=""></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
+
+    <main role="main" class="container">
+
+      <div class="starter-template">
+        
+
+
+        <table class="table table-hover ">
+          <thead class="thead-dark" style="text-align: center;">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col-2">nom</th>
+              <th scope="col-3">description</th>
+              <th scope="col">tarif heure</th>
+              <th scope="col">tarif jour</th>
+              <th scope="col">tarif etudiant</th>
+              <th scope="col">frais sans engagement</th>
+              <th scope="col">frais avec engagement</th>
+              <th scope="col">durée engagement</th>
+            </tr>
+          </thead>
+          <tbody >
+            <?php  
+              foreach ($listOfAbonnements as $key => $abonnement) {
+                echo '<tr><td style="text-align:center">'.$abonnement['id'].'</td>';
+                echo '<td style="text-align:center">'.$abonnement['name'].'</td>';
+                echo '<td>'.$abonnement['description'].'</td>';
+                echo '<td style="text-align:center">'.$abonnement['hourRate'].'</td>';
+                echo '<td style="text-align:center">'.$abonnement['dayRate'].'</td>';
+                echo '<td style="text-align:center">'.$abonnement['studentRate'].'</td>';
+                echo '<td style="text-align:center">'.$abonnement['engagementRate'].'</td>';
+                echo '<td style="text-align:center">'.$abonnement['notEngagementRate'].'</td>';
+                echo '<td style="text-align:center">'.$abonnement['engagementTime'].'</td>';
+                echo '</tr>';
+
+              }
+
+            ?>
+          </tbody>
+        </table>
+
+
+
+      </div>
+
+    </main><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../../../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ script.js
@@ -0,0 +1,128 @@
+function verifForm(id){
+	var name = document.getElementById("inputName"+id);
+	var description = document.getElementById("inputDescription"+id);
+	var hourRate = document.getElementById("inputHourRate"+id);
+	var dayRate = document.getElementById("inputDayRate"+id);
+	var studentRate = document.getElementById("inputStudentRate"+id);
+	var engagementRate = document.getElementById("inputEngagementRate"+id);
+	var notEngagementRate = document.getElementById("inputNoEngagementRate"+id);
+	var engagementTime = document.getElementById("inputEngagementTime"+id);
+
+
+	var errors = 0;
+	var isModif;
+	var nameError = "";
+	var hourRateError = "";
+	var dayRateError = "";
+	var engagementRateError = "";
+	var studentRateError = "";
+	var notEngagementRateError = "";
+	var engagementTimeError = "";
+
+
+
+	if(id > 0){
+		isModif = "modification";
+	}else{
+		isModif = "création";
+	}
+
+	if(name.value.length < 8){
+		errors++;
+		nameError = "\n- Le titre est trop court"; 
+	}else if(name.value.length > 50){
+		errors++;
+		nameError = "\n- Le titre est trop long"; 
+	}
+
+	if(isNaN(parseInt(hourRate.value))){
+		errors++;
+		hourRateError = "\n- valeur de hour Rate invalide"; 
+	}
+
+	if(isNaN(parseInt(dayRate.value))){
+		errors++;
+		dayRateError = "\n- valeur de day Rate invalide";
+	}
+
+	if(isNaN(parseInt(studentRate.value))){
+		errors++;
+		studentRateError = "\n- valeur de student Rate invalide";
+	}
+
+	if(isNaN(parseInt(engagementRate.value))){
+		errors++;
+		engagementRateError = "\n- valeur de engagement Rate invalide";
+	}
+
+	if(isNaN(parseInt(notEngagementRate.value))){
+		errors++;
+		notEngagementRateError = "\n- valeur de not engagement Rate invalide";
+	}
+
+	if(isNaN(parseInt(engagementTime.value))){
+		errors++;
+		engagementTimeError = "\n- valeur de engagement Time invalide";
+	}
+
+
+	if(errors > 0){
+
+		alert("Votre formulaire de "+isModif+" a une erreur, voici vos erreur : "+nameError+hourRateError+dayRateError+studentRateError+engagementRateError+notEngagementRateError+engagementTimeError+"\n merci de modifier les champs correspondant :)");
+	
+	}else{
+
+		var xhr = getXMLHttpRequest();
+
+		xhr.open("POST", "request.php", true);
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		xhr.send("id=" + id + "&name=" + name.value + "&description=" + description.value + "&hourRate=" + hourRate.value + "&dayRate=" + dayRate.value + "&studentRate=" + studentRate.value + "&engagementRate=" + engagementRate.value + "&notEngagementRate=" + notEngagementRate.value + "&engagementTime=" + engagementTime.value);
+
+/*		$.ajax({
+			url : 'requête.php',
+			type : 'POST',
+			data : 'id=' + id + '&description=' + description + 'hourRate=' + hourRate + '&dayRate=' + dayRate + 'studentRate=' + studentRate + '&engagementRate=' + engagementRate + 'notEngagementRate=' + notEngagementRate + '&engagementTime=' + engagementTime,
+			datatype : 'html',
+			success : function(){
+				alert("success");
+			},
+
+			error : function(){
+				alert("error");
+			}
+		});*/
+
+	}	
+
+
+	console.log(name.value);
+	console.log(description.value);
+	console.log(hourRate.value);
+	console.log(dayRate.value);
+	console.log(studentRate.value);
+	console.log(engagementRate.value);
+	console.log(notEngagementRate.value);
+	console.log(engagementTime.value);
+	console.log(isModif);
+}
+
+function getXMLHttpRequest() {
+	var xhr = null;
+	
+	if (window.XMLHttpRequest || window.ActiveXObject) {
+		if (window.ActiveXObject) {
+			try {
+				xhr = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch(e) {
+				xhr = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+		} else {
+			xhr = new XMLHttpRequest(); 
+		}
+	} else {
+		alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
+		return null;
+	}
+	
+	return xhr;
+}
